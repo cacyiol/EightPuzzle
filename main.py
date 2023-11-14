d569c57cc00d3d68616c5d2902b29dfc4c0d1489
cacyiol <77148278+cacyiol@users.noreply.github.com>
2023-11-14 20:53:45 +0800

    11/14 Preliminary completion of DFS algorithm implementation

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from DFS import *
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    DFSUtil(g, visited)
+    print("在当前深度下没有找到解，请尝试增加搜索深度")
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
